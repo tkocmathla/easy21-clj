fece8dbaa2e28cbfdedc42a501ec9164c7ae4a6c
Matt Grimm <matt.t.grimm@gmail.com>
2019-02-05 13:54:16 -0700

    Remove useless or

--- src/easy21/policies.clj
+++ src/easy21/policies.clj
@@ -2,7 +2,7 @@
   (:require [easy21.environment :as env]))
 
 (defn e-greedy [S Q N]
-  (let [n (+ (N [S :hit] 0) (or (N [S :stick] 0)))
+  (let [n (+ (N [S :hit] 0) (N [S :stick] 0))
         e (/ 100 (+ 100 n))
         qs (merge (select-keys Q [S :hit]) (select-keys Q [S :stick]))]
     (if (or (< (rand) e) (empty? qs))
